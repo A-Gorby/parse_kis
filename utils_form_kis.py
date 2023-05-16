6a5a1a7abdba08cb90d9c2816447651c6f111181
A-Gorby <123462010+A-Gorby@users.noreply.github.com>
2023-05-16 11:19:20 +0300

    Add files via upload

--- utils_form_kis.py
+++ utils_form_kis.py
@@ -1,6 +1,7 @@
 import ipywidgets as widgets
 from ipywidgets import Layout, Box, Label
 import os
+import re
 import pandas as pd
 
 # global fn_kis_file_drop_douwn, sheet_name_drop_down, col_name_drop_douwn, data_kis_source_dir
@@ -83,6 +84,19 @@ def form_esklp_dates(fn_list):
     esklp_dates = [re.findall(r'(?:\d\d\d\d\d\d\d\d)', fn) for fn in fn_list]
     esklp_dates = list(set([d[0] for d in esklp_dates if len(d) > 0]))
     return esklp_dates
+
+def param_form_znvlp_esklp_dicts(fn_list):
+    esklp_dates = form_esklp_dates(fn_list)
+    esklp_dates_dropdown = widgets.Dropdown( options=esklp_dates) #, value=None)
+    
+    form_item_layout = Layout(display='flex', flex_flow='row', justify_content='space-between')
+    check_box = Box([Label(value="Выберите дату ЕСКЛП справочника для использования:"), esklp_dates_dropdown], layout=form_item_layout) 
+    form_items = [check_box]
+    
+    form_znvlp_esklp_dicts = Box(form_items, layout=Layout(display='flex', flex_flow= 'column', border='solid 2px', align_items='stretch', width='50%')) #width='auto'))
+    # return form, fn_check_file_drop_douwn, fn_dict_file_drop_douwn, radio_btn_big_dict, radio_btn_prod_options, similarity_threshold_slider, max_entries_slider
+    return form_znvlp_esklp_dicts, esklp_dates_dropdown 
+    
 def param_form_kis_esklp_dicts(fn_list):
     esklp_dates = form_esklp_dates(fn_list)
     esklp_dates_dropdown = widgets.Dropdown( options=esklp_dates) #, value=None)
